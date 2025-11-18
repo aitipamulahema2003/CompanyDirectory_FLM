db27163e549e6778670b7ac020836c41e86e655a
aitipamulahema2003 <aitipamulahema2003@gmail.com>
2025-11-18 12:41:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const companies = require('./data/companies.json');
-app.get('/api/companies', (req, res) => { res.json(companies); });
+app.get('https://companydirectory-flm.onrender.com/api/companies', (req, res) => { res.json(companies); });
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
